e28ac852815f29df7dd0a2b97eb9454173e1bfc8
AlamoVinicius <alamo.cma22@gmail.com>
2023-01-17 22:33:01 -0300

    first commit

---
+++ src/components/atoms/Loading/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+import { View, Text, ActivityIndicator } from 'react-native';
+
+export const LoadingSpinner = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#09090A' }}>
+      <ActivityIndicator color="#7C3AED" />
+    </View>
+  );
+};
